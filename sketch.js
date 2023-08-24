234bf3df710556b5df808794a70745adfcc8c479
David <david.nguyen00@outlook.com>
2023-08-24 14:20:44 -0700

    Code now shows a 16x16 grid

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
-var existingDiv = document.getElementById("div_container");
-for (let i = 0; i < 16; i++) {
-    var squareDiv = document.createElement("div");
-    squareDiv.style.width = "100px";
-    squareDiv.style.height = "100px";
-    squareDiv.style.border = "1px solid black";
-    squareDiv.style.display = "inline-block";
-    existingDiv.appendChild(squareDiv);
+var parentDiv = document.getElementById("div_container");
+
+for (var i = 0; i < 16; i++) {
+  var row = document.createElement("div");
+  row.classList.add("grid-row");
+  parentDiv.appendChild(row);
+
+  for (var j = 0; j < 16; j++) {
+    var cell = document.createElement("div");
+    cell.classList.add("grid-cell");
+    parentDiv.children[i].appendChild(cell);
+  }
 }
-    
-    
-   
-   
 
