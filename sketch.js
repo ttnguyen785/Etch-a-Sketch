3f757bf3415e64870f9c7fc9c4407078979b9828
David <david.nguyen00@outlook.com>
2023-08-24 13:30:56 -0700

    Created first row of 16x16 grid needed

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var existingDiv = document.getElementById("div_container");
+for (let i = 0; i < 16; i++) {
+    var squareDiv = document.createElement("div");
+    squareDiv.style.width = "100px";
+    squareDiv.style.height = "100px";
+    squareDiv.style.border = "1px solid black";
+    squareDiv.style.display = "inline-block";
+    existingDiv.appendChild(squareDiv);
+}
+    
+    
+   
+   
+
