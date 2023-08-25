e2a11d6f50d096a5fb577a89ed417bd9988a8afb
David <david.nguyen00@outlook.com>
2023-08-25 12:40:36 -0700

    Color changing buttons: black and random added

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+let color = "black";
 document.addEventListener("DOMContentLoaded", function() {
-  createBoard(32);
-  console.log('Hi');
-
+  createBoard(16);
+  let btn_popup = document.querySelector("#popup");
+  btn_popup.addEventListener("click", function(){
+    let size = getSize();
+    createBoard(size);
+  })
 })
  
 function createBoard(size) {
@@ -13,8 +17,34 @@ function createBoard(size) {
   let numDivs = size * size;
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div")
-    div.style.border = "1px solid black" 
+    div.addEventListener("mouseover",colorDiv)
     board.insertAdjacentElement("beforeend", div);
   }
+}
+
+function getSize() {
+  let input = prompt("What will be the size of the board?")
+  let message = document.getElementById("message")
+  if (input == "") {
+    message.innerText = "Please prove a number";
+  } else if (input < 0 || input > 100) {
+    message.innerText = "Please prove a number between 1 and 100";
+  }
+  else{
+    return input;
+  }
+}
+
+function colorDiv(){
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+  }
+  else {
+    this.style.backgroundColor = 'black';
+  }
+}
+
+function setColor(colorChoice){
+  color = colorChoice;
 }
 
